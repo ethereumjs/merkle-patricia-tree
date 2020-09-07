4f0e1503f5324243df304e4fc76930e9821f69ba
Jochem Brouwer <jochembrouwer96@gmail.com>
2020-09-07 20:31:08 +0200

    cleanup the code and document

--- src/prioritizedTaskExecutor.ts
+++ src/prioritizedTaskExecutor.ts
@@ -36,12 +36,13 @@ export class PrioritizedTaskExecutor {
    */
   async execute(priority: number, fn: Function) {
     let self = this
-    function runTask() {
+    let runTask = () => {
       self.currentPoolSize++
       fn(async () => {
+        // this callback function can be `await`ed
         self.currentPoolSize--
         if (self.queue.length > 0) {
-          await self.lock.acquire()
+          await self.lock.acquire() // we need the lock to be unlocked, because we are editing the queue.
           const item = self.queue.shift()
           await self.lock.signal()
           await self.execute(item!.priority, item!.fn)
@@ -61,38 +62,40 @@ export class PrioritizedTaskExecutor {
         let mid = () => {
           return Math.floor(left + (right - left) / 2)
         }
+        let insert = (index: number) => {
+          if (this.currentPoolSize < this.maxPoolSize) {
+            runTask()
+          } else {
+            this.queue.splice(index, 0, { priority, fn })
+          }
+        }
         while (true) {
           // note that there is a special case: it could be that during sorting, a Task is finished (reducing currentPoolSize by 1), but this Task was not yet inserted
           // therefore, if we want to insert the item we explicitly check that we indeed should Queue it, if not, we execute it and do not insert it.
           let index = mid()
           let value = this.queue[index].priority
           if (value == priority) {
-            if (this.currentPoolSize < this.maxPoolSize) {
-              runTask()
-            } else {
-              this.queue.splice(index, 0, { priority, fn })
-            }
+            // we have found the priority value and can thus insert the task at this index.
+            insert(index)
             break
           }
           if (left == right) {
-            if (this.currentPoolSize < this.maxPoolSize) {
-              runTask()
-            } else {
-              this.queue.splice(left, 0, { priority, fn })
-            }
+            // we have only one element left, this means that the items left of this index have a higher priority and the items right have a lower priority
+            // we thus insert it at this index
+            insert(left)
             break
           }
           if (value > priority) {
+            // we know everything left of the index has a higher priority, so we do not need to consider these items anymore
             left = index + 1
           } else {
+            // we know everything right of the index has a lowre priority, so we do not need to consider these items anymore
             right = index - 1
           }
           if (left > right) {
-            if (this.currentPoolSize < this.maxPoolSize) {
-              runTask()
-            } else {
-              this.queue.splice(left, 0, { priority, fn })
-            }
+            // this could happen, for instance, if left = 0 right = 1 (the index is 0) and the item has a lower priority.
+            // then right = -1, so we should insert at the left index
+            insert(left)
             break
           }
         }
